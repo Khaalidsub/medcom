112b7ad44b9274004fdc3a7452c1de7060bcf6d2
adeenrabbani1 <52000430+adeenrabbani1@users.noreply.github.com>
2020-03-16 22:30:23 +0800

    hospRegistrationFinal

--- health_app/lib/src/app.dart
+++ health_app/lib/src/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:health_app/src/screens/home.dart';
+import 'package:health_app/src/screens/hospRegistration.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      home: Home(),
+      home: HospRegistration(),
     );
   }
 }
