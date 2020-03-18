20bd2c690d9fb11b7b35c2053878120a6cced43a
Khaalidsub <khaalidsubaan@gmail.com>
2020-03-18 23:59:39 +0800

    home patient

--- health_app/lib/src/screens/home.dart
+++ health_app/lib/src/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:health_app/src/screens/widgets/app_nav.dart';
 import 'package:health_app/src/screens/widgets/bottom_navigation.dart';
 import 'package:health_app/src/screens/widgets/chart.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:vector_math/vector_math.dart' as math;
 
 class Home extends StatefulWidget {
   final Widget child;
@@ -43,40 +44,99 @@ class _HomeState extends State<Home> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                    height: 120,
-                    width: width * 0.29,
-                    child: Card(
-                      color: Color(0xffFFC4C4),
-                      child: Icon(
-                        FontAwesomeIcons.algolia,
-                        size: 50,
+                  GestureDetector(
+                    onTap: () => showDialogue(context),
+                    child: Container(
+                      height: 120,
+                      width: width * 0.29,
+                      child: Card(
+                        color: Color(0xffFFC4C4),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.userMd,
+                              color: Colors.grey,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Appointments',
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  Container(
-                    height: 120,
-                    width: width * 0.29,
-                    child: Card(
-                      color: Color(0xffF5DD9C),
-                      child: Icon(
-                        FontAwesomeIcons.alipay,
-                        size: 50,
+                  GestureDetector(
+                    onTap: () => showDialogue(context),
+                    child: Container(
+                      height: 120,
+                      width: width * 0.29,
+                      child: Card(
+                        color: Color(0xffF5DD9C),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.hospital,
+                              color: Colors.grey,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Hospitals',
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  Container(
-                    height: 120,
-                    width: width * 0.29,
-                    child: Card(
-                      color: Color(0xff8DDEDE),
-                      child: Icon(
-                        FontAwesomeIcons.adversal,
-                        size: 50,
+                  GestureDetector(
+                    child: Container(
+                      height: 120,
+                      width: width * 0.29,
+                      child: Card(
+                        color: Color(0xff8DDEDE),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.inbox,
+                              color: Colors.grey,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'updates',
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Statistics',
+                style: TextStyle(
+                  fontSize: 23,
+                ),
+              ),
+              SizedBox(
+                height: 10,
               ),
               Container(height: 400, child: Charts()),
             ],
@@ -84,6 +144,56 @@ class _HomeState extends State<Home> {
         ),
       ),
       bottomNavigationBar: BottomNavigation(),
+    );
+  }
+
+  Future showDialogue(BuildContext context) {
+    return showGeneralDialog(
+      context: context,
+      pageBuilder: (context, anim1, anim2) {},
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.4),
+      barrierLabel: '',
+      transitionBuilder: (context, anim1, anim2, child) {
+        return Transform.scale(
+          scale: anim1.value,
+          child: SimpleDialog(
+            contentPadding: EdgeInsets.all(0),
+            titlePadding: EdgeInsets.all(0),
+            shape: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            backgroundColor: Color(0xffFFFEDE),
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: 500,
+                child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+                      color: Colors.yellow[200 * index],
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: Image.asset('assets/images/clin.png'),
+                            title: Center(
+                              child: Text('Hospital ${index}'),
+                            ),
+                            trailing: Icon(FontAwesomeIcons.arrowDown),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      transitionDuration: Duration(milliseconds: 300),
     );
   }
 }
--- health_app/lib/src/screens/widgets/chart.dart
+++ health_app/lib/src/screens/widgets/chart.dart
@@ -44,7 +44,7 @@ class _ChartsState extends State<Charts> {
         data: data1,
         fillPatternFn: (_, __) => charts.FillPatternType.solid,
         fillColorFn: (Pollution pollution, _) =>
-            charts.ColorUtil.fromDartColor(Color(0xff990099)),
+            charts.ColorUtil.fromDartColor(Color(0xffFFC4C4)),
       ),
     );
 
@@ -56,7 +56,7 @@ class _ChartsState extends State<Charts> {
         data: data2,
         fillPatternFn: (_, __) => charts.FillPatternType.solid,
         fillColorFn: (Pollution pollution, _) =>
-            charts.ColorUtil.fromDartColor(Color(0xff109618)),
+            charts.ColorUtil.fromDartColor(Color(0xffF5DD9C)),
       ),
     );
 
@@ -68,7 +68,7 @@ class _ChartsState extends State<Charts> {
         data: data3,
         fillPatternFn: (_, __) => charts.FillPatternType.solid,
         fillColorFn: (Pollution pollution, _) =>
-            charts.ColorUtil.fromDartColor(Color(0xffff9900)),
+            charts.ColorUtil.fromDartColor(Color(0xff8DDEDE)),
       ),
     );
 
@@ -96,40 +96,47 @@ class _ChartsState extends State<Charts> {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: <Widget>[
-        charts.BarChart(
-          _seriesData,
-          animate: true,
-          barGroupingType: charts.BarGroupingType.grouped,
-          behaviors: [charts.SeriesLegend()],
-          animationDuration: Duration(seconds: 5),
-        ),
-        charts.PieChart(
-          _seriesPieData,
-          animate: true,
-          animationDuration: Duration(seconds: 5),
-          behaviors: [
-            new charts.DatumLegend(
-              outsideJustification: charts.OutsideJustification.endDrawArea,
-              horizontalFirst: false,
-              desiredMaxRows: 2,
-              cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
-              entryTextStyle: charts.TextStyleSpec(
-                  color: charts.MaterialPalette.purple.shadeDefault,
-                  fontFamily: 'Georgia',
-                  fontSize: 11),
-            )
+    return Card(
+      elevation: 0,
+      margin: EdgeInsets.all(0),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: PageView(
+          children: <Widget>[
+            charts.BarChart(
+              _seriesData,
+              animate: true,
+              barGroupingType: charts.BarGroupingType.grouped,
+              behaviors: [charts.SeriesLegend()],
+              animationDuration: Duration(seconds: 5),
+            ),
+            charts.PieChart(
+              _seriesPieData,
+              animate: true,
+              animationDuration: Duration(seconds: 5),
+              behaviors: [
+                new charts.DatumLegend(
+                  outsideJustification: charts.OutsideJustification.endDrawArea,
+                  horizontalFirst: false,
+                  desiredMaxRows: 2,
+                  cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                  entryTextStyle: charts.TextStyleSpec(
+                      color: charts.MaterialPalette.purple.shadeDefault,
+                      fontFamily: 'Georgia',
+                      fontSize: 11),
+                )
+              ],
+              defaultRenderer: new charts.ArcRendererConfig(
+                arcWidth: 100,
+                arcRendererDecorators: [
+                  new charts.ArcLabelDecorator(
+                      labelPosition: charts.ArcLabelPosition.inside)
+                ],
+              ),
+            ),
           ],
-          defaultRenderer: new charts.ArcRendererConfig(
-            arcWidth: 100,
-            arcRendererDecorators: [
-              new charts.ArcLabelDecorator(
-                  labelPosition: charts.ArcLabelPosition.inside)
-            ],
-          ),
         ),
-      ],
+      ),
     );
   }
 }
