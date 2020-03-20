ce8237b5e8bab06352612a8ca0ed6d574aa14547
Khaalidsub <khaalidsubaan@gmail.com>
2020-03-20 08:15:25 +0800

    add patient history

--- health_app/lib/src/app.dart
+++ health_app/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:health_app/src/screens/hospitalScreens/home.dart';
+import 'package:health_app/src/screens/hospitalScreens/hospRegistration.dart';
 import 'package:health_app/src/screens/patientScreens/history.dart';
 import 'package:health_app/src/screens/patientScreens/home.dart';
 
--- health_app/lib/src/screens/patientScreens/history.dart
+++ health_app/lib/src/screens/patientScreens/history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:health_app/src/screens/widgets/app_nav.dart';
 import 'package:health_app/src/screens/widgets/bottom_navigation.dart';
 
@@ -15,11 +16,19 @@ class PatientHistory extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(10),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('Dates'),
               Container(
-                  height: 100,
+                margin: EdgeInsets.only(top: 20, bottom: 15),
+                child: Text(
+                  'Dates',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              Container(
+                  height: 120,
                   width: width * 1,
+                  margin: EdgeInsets.only(bottom: 20),
                   child: ListView.builder(
                       itemCount: 6,
                       scrollDirection: Axis.horizontal,
@@ -31,12 +40,17 @@ class PatientHistory extends StatelessWidget {
                             elevation: 0,
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(20)),
-                            color: Colors.amber,
+                            color: Color(0xffF5DD9C),
                             child: Center(child: Text('$index')),
                           ),
                         );
                       })),
-              Text('Statistics'),
+              Container(
+                child: Text(
+                  'Statistics',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
               Container(
                 width: width,
                 height: 120,
@@ -79,31 +93,57 @@ class PatientHistory extends StatelessWidget {
                   ),
                 ),
               ),
-              Text('Apppointments'),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Apppointment',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               Container(
                 width: width,
                 height: 250,
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Container(
                       height: 250,
-                      width: width * 0.5,
+                      width: width * 0.45,
                       child: Card(
                         elevation: 0,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Image.asset('assets/images/clin.png'),
-                                Text('Hospitals')
-                              ],
-                            ),
-                            Text('Diagnosis'),
-                          ],
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10, top: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Image.asset('assets/images/clin.png'),
+                                  Text(
+                                    'Hospital',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                'Diagnosis',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -111,25 +151,37 @@ class PatientHistory extends StatelessWidget {
                       width: width * 0.44,
                       height: 250,
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           Container(
                             width: width * 0.44,
-                            height: 250 * 0.5,
+                            height: 250 * 0.45,
                             child: Card(
                               elevation: 0,
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(20)),
-                              child: Text('height'),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    FontAwesomeIcons.userMd,
+                                    size: 35,
+                                  ),
+                                  Text('Name'),
+                                ],
+                              ),
                             ),
                           ),
                           Container(
                             width: width * 0.44,
-                            height: 250 * 0.5,
+                            height: 250 * 0.45,
                             child: Card(
                               elevation: 0,
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(20)),
-                              child: Text('height'),
+                              child: Center(
+                                child: Text('IDK'),
+                              ),
                             ),
                           )
                         ],
