85f225e2186c4779f409e621e2a109188b601db4
adeenrabbani1 <52000430+adeenrabbani1@users.noreply.github.com>
2020-03-24 14:50:52 +0800

    alignment.

--- health_app/lib/src/screens/patientScreens/profile.dart
+++ health_app/lib/src/screens/patientScreens/profile.dart
@@ -9,6 +9,10 @@ class PatientProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
     double width = queryData.size.width;
+    double height = queryData.size.height;
+    print('the value is ${queryData.size.aspectRatio}');
+    print(width / height);
+
     return Material(
       child: Scaffold(
         backgroundColor: Colors.grey[100],
@@ -24,132 +28,147 @@ class PatientProfile extends StatelessWidget {
           ],
           backgroundColor: Colors.blue[700],
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
-                    child: Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundColor: Colors.lime[400],
-                          backgroundImage:
-                              AssetImage("assets/images/unknownPerson.jpg"),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.lime[400],
+                      backgroundImage:
+                          AssetImage("assets/images/unknownPerson.jpg"),
 
-                          radius: 60,
-                          //adding a background Image later.
-                        ),
-                        SizedBox(
-                          width: 50,
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              "Name : ",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text("ID : ",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                )),
-                            Text("Email : ",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                )),
-                            Text("Gender : ",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                )),
-                          ],
-                        )
-                      ],
+                      radius: 60,
+                      //adding a background Image later.
                     ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                  width: width * 0.9,
-                  height: queryData.size.height * .4,
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    //*** Adjust overflow thing in the container**/
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "Blood Type: ",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          Text(
-                            'A+',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text("Allergies: Lorem ipsum",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20))
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text("Location: Lorem ipsum",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20))
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text("Contact: Lorem Ipsum",
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20)),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 0,
-                      ),
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(
-                              (30),
-                            ),
-                          ),
-                        ),
-                        onPressed: null,
-                        disabledColor: Colors.blue[700],
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        child: Text(
-                          "Update Details",
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          "Name : ",
                           style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
                         ),
+                        Text("ID : ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            )),
+                        Text("Email : ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            )),
+                        Text("Gender : ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+              width: width * 0.9,
+              height: queryData.size.height * .4,
+              padding: EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //*** Adjust overflow thing in the container**/
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Blood Type: ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 2.5 * height / 100),
+                      ),
+                      Text(
+                        'A+',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 2.5 * height / 100),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  Row(
+                    children: <Widget>[
+                      Text("Allergies:",
+                          overflow: TextOverflow.fade,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 2.5 * height / 100)),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "Muhammad Adeen  Faizan",
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                                fontSize: 2.5 *
+                                    height /
+                                    100), //depending on block size
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text("Location: Lorem ipsum",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 2.5 * height / 100))
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text("Contact: Lorem Ipsum",
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 2.5 * height / 100)),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          (30),
+                        ),
+                      ),
+                    ),
+                    onPressed: null,
+                    disabledColor: Colors.blue[700],
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 6 * width / 100,
+                        vertical: 2 * height / 100),
+                    child: Text(
+                      "Update Details",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 2.5 * height / 100),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+          ],
         ),
         bottomNavigationBar: BottomNavigation(),
       ),
