6e2dc43be7636f133ad70cc0fbc8c08ab77a825e
Khaalidsub <khaalidsubaan@gmail.com>
2020-04-03 13:27:51 +0800

    splash screen

--- health_app/lib/src/app.dart
+++ health_app/lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:health_app/src/screens/login.dart';
+import 'package:health_app/src/screens/splash_screen.dart';
 import 'package:health_app/src/screens/welcoming_screen.dart';
 
 class App extends StatefulWidget {
@@ -13,7 +14,7 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      home: WelcomingScreen(),
+      home: SplashScreen(),
     );
   }
 }
