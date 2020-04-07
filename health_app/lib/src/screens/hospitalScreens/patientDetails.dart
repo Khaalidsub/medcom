38eb3aca7f543df19c6213b98481764a708f57cb
adeenrabbani1 <52000430+adeenrabbani1@users.noreply.github.com>
2020-04-07 20:50:25 +0800

    Tabs View in the Patient Details

--- health_app/lib/src/screens/hospitalScreens/patientDetails.dart
+++ health_app/lib/src/screens/hospitalScreens/patientDetails.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:health_app/src/screens/hospitalScreens/appointmentHistoryContent.dart';
+import 'package:health_app/src/screens/hospitalScreens/latestAppointementContent.dart';
 import 'package:health_app/src/screens/widgets/app_nav.dart';
 
 class PatientDetails extends StatefulWidget {
@@ -8,183 +10,109 @@ class PatientDetails extends StatefulWidget {
   _PatientDetailsState createState() => _PatientDetailsState();
 }
 
-class _PatientDetailsState extends State<PatientDetails> {
-  final items = List<String>.generate(
-      10, (i) => "Brain Cancer. Must be taken care of sensitive mdication$i");
+class _PatientDetailsState extends State<PatientDetails>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new TabController(length: 2, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
     double width = queryData.size.width;
     double height = queryData.size.height;
-    return Material(
-      child: Scaffold(
-        appBar: AppNav(
-          appBar: AppBar(),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(
-                  top: 15, bottom: 10), //test later with diff screen size
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/unknownPerson.jpg'),
-                radius: 60,
+    return Scaffold(
+      appBar: AppNav(
+        appBar: AppBar(),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                top: 15, bottom: 10), //test later with diff screen size
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/unknownPerson.jpg'),
+              radius: 60,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Text(
+              'Justin Bieber',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 3 * height / 100,
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(bottom: 15),
-              child: Text(
-                'Justin Bieber',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 3 * height / 100,
-                ),
-              ),
-            ),
-            RaisedButton(
-              onPressed: null, //add appointment Functionality goes here!
-              disabledColor: Colors.blue[600],
-              child: FittedBox(
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Add Appointment',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
-                ),
-              ),
-            ),
-            //*********************************************************************
-            //this block of code might be set visible if everybody agrees to put
-            //new appointments in the same screen
-//**********************************************************************
-            //Container(
-            //  margin: EdgeInsets.only(top: 10, bottom: 5),
-            //  child: Align(
-            //    alignment: Alignment.centerLeft,
-            //    child: Text(
-            //      "New Appointments",
-            //      style: TextStyle(
-            //        fontWeight: FontWeight.bold,
-            //        fontSize: 20,
-            //      ),
-            //    ),
-            //  ),
-            //),
-            //Expanded(
-            //  flex: 1,
-            //  child: ListView.builder(
-            //      itemCount: 0,
-            //      itemBuilder: (BuildContext context, int index) {
-            //        return Card(
-            //          elevation: 10,
-            //          color: Colors.white,
-            //          child: ListTile(
-            //            leading: Icon(FontAwesomeIcons.calendarAlt),
-            //            trailing: IconButton(
-            //                icon: Icon(Icons.menu),
-            //                onPressed:
-            //                    null), //on press shows a small action dialog, whatsapp for reference
-            //            title: Text(
-            //              "Sunday 2/10/1999",
-            //              style: TextStyle(fontWeight: FontWeight.bold),
-            //            ),
-            //            subtitle: Text(
-            //                "Brain tumor with internal bleeding. Must need home care and medicins need to be taken care of."),
-            //          ),
-            //        );
-            //      }),
-            //),
-            Container(
-              margin: EdgeInsets.only(top: 10, bottom: 5),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Appoinment History",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+          ),
+          RaisedButton(
+            onPressed: null, //add appointment Functionality goes here!
+            disabledColor: Colors.blue[600],
+            child: FittedBox(
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
                   ),
-                ),
+                  Text(
+                    'Add Appointment',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  )
+                ],
               ),
             ),
-            Expanded(
-              flex: 4,
-              child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Dismissible(
-                      direction: DismissDirection.endToStart,
-                      confirmDismiss: null,
-                      key: UniqueKey(), //unique shit
-                      background: Container(
-                        padding: EdgeInsets.only(right: 10),
-                        color: Colors.red[800],
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            "Delete Record",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
-                          ),
-                        ),
-                      ),
-                      onDismissed: (direction) {
-                        setState() {
-                          items.removeAt(index);
-                          //remove the items
-                          //also from the back end
-                        }
-
-                        Scaffold.of(context).showSnackBar(
-                          SnackBar(
-                            content: Row(
-                              children: <Widget>[
-                                Text("Record Deleted"),
-                                IconButton(
-                                    icon: Icon(Icons.undo), onPressed: null)
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      child: Card(
-                        elevation: 10,
-                        color: Colors.white,
-                        child: ListTile(
-                          leading: Icon(FontAwesomeIcons.calendarAlt),
-                          trailing: IconButton(
-                              icon: Icon(Icons.menu),
-                              onPressed:
-                                  null), //on press shows a small action dialog, whatsapp for reference
-                          title: Text(
-                            "Sunday 2/10/2020",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text(items[index]),
-                        ),
-                      ),
-                    );
-                  }),
-            )
-          ],
-        ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.white),
+            child: TabBar(
+              indicatorColor: Colors.blueAccent,
+              labelColor: Colors.black,
+              controller: _controller,
+              tabs: [
+                Tab(
+                  icon: const Icon(
+                    FontAwesomeIcons.calendarCheck,
+                    color: Colors.blueAccent,
+                  ),
+                  text: "Latest Appointments",
+                ),
+                Tab(
+                  icon: const Icon(
+                    FontAwesomeIcons.history,
+                    color: Colors.blueAccent,
+                  ),
+                  text: 'History',
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              // height: 80.0,
+              child: TabBarView(
+                controller: _controller, //to control the sync of tabs and views
+                children: <Widget>[
+                  //separate widgets that holds contents respectively for both tabs
+                  LatestAppointmenContent(),
+                  AppointmenContent(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- health_app/lib/src/screens/hospitalScreens/patientList.dart
+++ health_app/lib/src/screens/hospitalScreens/patientList.dart
@@ -6,40 +6,38 @@ import 'package:health_app/src/screens/widgets/app_nav.dart';
 class PatientList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        appBar: AppNav(
-          appBar: AppBar(),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: ListView.builder(
-              addAutomaticKeepAlives: false,
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => PatientDetails(),
-                          ),
-                        );
-                      }, //goes to patient full data page
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.black,
-                        backgroundImage: AssetImage('assets/images/ill.png'),
-                      ),
-                      title: Text("Muhammad adeen Rabbani"),
-                      subtitle: Text("Age:"),
-                      trailing: IconButton(
-                        onPressed: null, //some crud operation on the data
-                        icon: Icon(Icons.menu),
-                      )),
-                );
-              }),
-        ),
+    return Scaffold(
+      //appBar: AppNav(
+      //  appBar: AppBar(),
+      //),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+            addAutomaticKeepAlives: false,
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: ListTile(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PatientDetails(),
+                        ),
+                      );
+                    }, //goes to patient full data page
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.black,
+                      backgroundImage: AssetImage('assets/images/ill.png'),
+                    ),
+                    title: Text("Muhammad adeen Rabbani"),
+                    subtitle: Text("Age:"),
+                    trailing: IconButton(
+                      onPressed: null, //some crud operation on the data
+                      icon: Icon(Icons.menu),
+                    )),
+              );
+            }),
       ),
     );
   }
