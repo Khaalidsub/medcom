295e5e9431ea2693731e0d6b40ffe2f4a6bfdd9c
Khaalidsub <khaalidsubaan@gmail.com>
2020-06-08 05:49:19 +0800

    refactor streamUser for bloc

--- health_app/lib/src/blocs/patient_navigation_bloc.dart
+++ health_app/lib/src/blocs/patient_navigation_bloc.dart
@@ -1,13 +1,3 @@
-import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:health_app/src/models/user.dart';
-import 'package:health_app/src/services/repository.dart';
+import 'package:health_app/src/blocs/stream_user_bloc.dart';
 
-class PatientNavigationBloc extends BlocBase {
-  String uid;
-  Stream<User> get streamUserData => Repository().userData.asStream();
-  @override
-  void dispose() async {
-    await streamUserData.drain();
-    super.dispose();
-  }
-}
+class PatientNavigationBloc extends StreamUserBloc {}
---
+++ health_app/lib/src/blocs/stream_user_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:health_app/src/models/user.dart';
+import 'package:health_app/src/services/repository.dart';
+
+abstract class StreamUserBloc extends BlocBase {
+  Stream<User> get streamUser => Repository().user;
+  Stream<User> get streamUserData => Repository().userData.asStream();
+  Future<User> userStream() async {
+    try {
+      return streamUserData.first;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  @override
+  void dispose() async {
+    await streamUser.drain();
+    await streamUserData.drain();
+    super.dispose();
+  }
+}
