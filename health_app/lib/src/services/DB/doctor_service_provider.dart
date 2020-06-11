677c29775f9abbac693daadb63f42dec3f231497
Khaalidsub <khaalidsubaan@gmail.com>
2020-06-11 22:23:37 +0800

    Create doctor_service_provider.dart

---
+++ health_app/lib/src/services/DB/doctor_service_provider.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:health_app/src/models/doctor.dart';
+
+class DoctorServiceProvider {
+  final CollectionReference doctorCollection =
+      Firestore.instance.collection('doctor_list');
+  String documentId;
+  String hospitalId;
+  DoctorServiceProvider({this.documentId, this.hospitalId});
+
+  ///get the doctor
+  Stream<Doctor> get doctor {
+    return doctorCollection
+        .document(documentId)
+        .snapshots()
+        .map(_doctorDataFromSnap);
+  }
+
+  ///get the doctors for a specific hospital
+  Stream<List<Doctor>> get doctorList {
+    return doctorCollection
+        .where('hospitalId', isEqualTo: hospitalId)
+        .snapshots()
+        .map(_doctorListFromSnap);
+  }
+
+  ///add doctor to the firestore
+  Future createDoctor(Doctor doctor) async {
+    await doctorCollection.document().setData(
+        {'name': doctor.name, 'specialization': doctor.specialization});
+  }
+
+  ///
+
+  //map it to an doctor object
+  Doctor _doctorDataFromSnap(DocumentSnapshot snap) {
+    Doctor doctor = new Doctor(
+        name: snap['name'], specialization: snap.data['specialization']);
+    return doctor;
+  }
+
+  ///map a list of doctors
+  List<Doctor> _doctorListFromSnap(QuerySnapshot snapshots) {
+    return snapshots.documents.map(_doctorDataFromSnap).toList();
+  }
+}
