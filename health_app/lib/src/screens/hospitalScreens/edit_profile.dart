f5e3623399c66993d4caa74a8ccaa3c2018ed58c
adeenrabbani1 <52000430+adeenrabbani1@users.noreply.github.com>
2020-06-23 16:47:13 +0800

    UI changes, to integrate with backend

--- health_app/lib/src/screens/hospitalScreens/edit_profile.dart
+++ health_app/lib/src/screens/hospitalScreens/edit_profile.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:health_app/src/blocs/hospital_blocs/hospital_edit_bloc.dart';
 import 'package:health_app/src/models/hospital.dart';
 import 'package:health_app/src/screens/widgets/app_nav.dart';
-import 'package:health_app/src/screens/widgets/input_field.dart';
+import 'package:health_app/src/screens/widgets/progress_bar.dart';
+import 'package:health_app/src/screens/widgets/stream_input_field.dart';
 
 class HospitalEditProfile extends StatefulWidget {
+//instantiating bloc
+
   final Hospital hospital;
   HospitalEditProfile(this.hospital);
   final TextEditingController phoneNumber = new TextEditingController();
@@ -12,6 +17,8 @@ class HospitalEditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<HospitalEditProfile> {
+  final hospitalEditBloc = HospitalEditBloc();
+  Hospital hospital;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,64 +26,121 @@ class _EditProfileState extends State<HospitalEditProfile> {
         appBar: AppBar(),
         name: 'Edit Profile',
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(30),
-              height: 200,
-              child: Hero(
-                tag: 'logo',
-                child: Image.asset(
-                  "assets/images/buildingIcon.png",
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(25, 5, 25, 25),
-              child: Wrap(
-                runSpacing: 20,
-                children: <Widget>[
-                  Reusablefield(
-                    controller: widget.phoneNumber,
-                    label: "${widget.hospital.phoneNumber ?? '+601103024'}",
-                    color: Colors.white,
-                    icon: Icon(Icons.phone),
-                    type: TextInputType.number,
-                    hint: 'e.g +6011616178',
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 0),
-              child: RaisedButton(
-                color: Colors.blueAccent,
-                onPressed: () async {
-                  if (widget.phoneNumber.text.isEmpty)
-                    widget.phoneNumber.text = widget.hospital.phoneNumber;
-                  widget.hospital.phoneNumber = widget.phoneNumber.text;
+      body: StreamBuilder<Object>(
+          stream: hospitalEditBloc.streamUserData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              hospital = snapshot.data;
+              return SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(30),
+                      height: 200,
+                      child: Hero(
+                        tag: 'logo',
+                        child: Image.asset(
+                          "assets/images/buildingIcon.png",
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(25, 5, 25, 25),
+                      child: Wrap(
+                        runSpacing: 20,
+                        children: <Widget>[
+                          Reusablefield(
+                            stream: hospitalEditBloc.name,
+                            onChangeFunction: hospitalEditBloc.changeName,
+                            label: "${widget.hospital.name}",
+                            color: Colors.white,
+                            icon: Icon(Icons.person),
+                            type: TextInputType.text,
+                            hint: 'e.g James Smith',
+                          ),
+                          Reusablefield(
+                            stream: hospitalEditBloc.phoneNumber,
+                            onChangeFunction:
+                                hospitalEditBloc.changePhoneNumber,
+                            label: "${widget.hospital.phoneNumber}",
+                            color: Colors.white,
+                            icon: Icon(Icons.phone),
+                            type: TextInputType.number,
+                            hint: 'e.g +6011616178',
+                          ),
+                          Reusablefield(
+                            stream: hospitalEditBloc.email,
+                            onChangeFunction: hospitalEditBloc.changeEmail,
+                            //controller: widget.phoneNumber,
+                            label: "${widget.hospital.email}",
+                            color: Colors.white,
+                            icon: Icon(Icons.mail),
+                            type: TextInputType.emailAddress,
+                            hint: 'e.g user@gmail.com',
+                          ),
+                          Reusablefield(
+                            stream: hospitalEditBloc.numOfBeds,
+                            onChangeFunction: hospitalEditBloc.changeEmail,
+                            //controller: widget.phoneNumber,
+                            label: "${widget.hospital.numOfBeds}",
+                            color: Colors.white,
+                            icon: Icon(FontAwesomeIcons.bed),
+                            type: TextInputType.number,
+                            hint: 'e.g 30',
+                          ),
+                          Reusablefield(
+                            //wcontroller: widget.phoneNumber,
+                            stream: hospitalEditBloc.address,
+                            onChangeFunction: hospitalEditBloc.changeAddress,
+                            label: "${widget.hospital.address}",
+                            color: Colors.white,
+                            icon: Icon(FontAwesomeIcons.addressBook),
+                            type: TextInputType.text,
+                            hint: 'e.g George town London',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 0),
+                      child: RaisedButton(
+                        color: Colors.blueAccent,
+                        onPressed: () async {
+                          //so when user updates, I want to trigger backend operation.
+                          //update the UI
+                          if (widget.phoneNumber.text.isEmpty)
+                            widget.phoneNumber.text =
+                                widget.hospital.phoneNumber;
+                          widget.hospital.phoneNumber = widget.phoneNumber.text;
 
-                  await buildShowDialog(context);
-                  Navigator.pop(context,
-                      widget.hospital); //sending back the updated data...
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
-                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                disabledColor: Colors.blueAccent,
-                child: Text(
-                  "Update",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
+                          await buildShowDialog(context);
+                          Navigator.pop(
+                              context,
+                              widget
+                                  .hospital); //sending back the updated data...
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                        disabledColor: Colors.blueAccent,
+                        child: Text(
+                          "Update",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+              );
+            }
+            return ProgressBar(
+              color: Colors.blueAccent,
+            );
+          }),
     );
   }
 
